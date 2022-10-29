1d7f7cabcf0e461b584404a5a438b875f3912168
AashvikD <aashvik.dhingra@gmail.com>
2022-10-29 22:39:59 +0530

    Final change Final change with both members' approval

--- routes.py
+++ routes.py
@@ -31,9 +31,9 @@ class PostForm(FlaskForm):
     title = StringField('Title: ', validators = [InputRequired(), Length(min=2, max=50)])
     description = StringField('Description: ', validators = [Length(max=300)])
     tags = StringField('Tags: ', validators = [Length(max=300)])
-    # sell = StringField('To Be Sold or Not: ', validators = [Length(max=300)])
     price = StringField('Price: ', validators = [Length(max=6)])
     submit = SubmitField("Post Artwork")
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = ''
@@ -41,13 +41,16 @@ app.config.from_pyfile('config.py')
 username = ""
 user_level = 0
 
+
 @app.route('/')
 def index_start():
     return community()
 
+
 @app.route('/index')
 def index():
     return community()
+
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
@@ -68,26 +71,17 @@ def register():
         
         file = request.files['pfp']
         filename = ""
-        print(file.filename, str(file.filename.split(".")[-1]))
         if str(file.filename.split(".")[-1]) in ["png", "jpg", "jpeg"]:
-            print("Yes! It reached!")
             filename = "static\\images\\profile_pics\\" + user_to_create["Username"] + "." + str(file.filename.split(".")[-1])
-            print(filename)
             file.save(filename)
-            print('file uploaded successfully')
         else:
             errors_lst.append("File: " + "Please upload a file with the extention .png, .jpg or .jpeg only")
-
-        print(errors_lst)
-
-            
-        if len(errors_lst) == 0:                                                                          
-            print(request.files['pfp'])
+  
+        if len(errors_lst) == 0:  
 
             user_to_create_command_str = "INSERT INTO userinfo VALUES(NULL, '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (filename, user_to_create["Username"], user_to_create["Name"], user_to_create["Email"], user_to_create["Password"], user_to_create["qualifications"], user_to_create["interests"], "basic")
             database_script.execute_userinfo(user_to_create_command_str)
             session["Username"] = user_to_create["Username"]
-            print(view_userinfo())
             return redirect(url_for('profile', login="1"))
             
     if register_form.errors != {}:
@@ -95,7 +89,6 @@ def register():
             errors_lst.append(field_name[0].upper() + field_name[1:] + ": " + error_msg[0])
 
     return render_template("register.html", register_form = register_form, errors_lst = errors_lst, login="0")
-
 
 
 @app.route('/login', methods=["POST", "GET"])
@@ -119,7 +112,6 @@ def signin():
     return render_template("login.html", signin_form=signin_form, error_text = error_text, login="0")
 
 
-
 @app.route('/profile', methods=["POST", "GET"])
 def profile():
     if session["Username"] != None:
@@ -131,12 +123,7 @@ def profile():
             if row[2] == username:
                 profile = row
         
-        print(profile)
-        # print('\\'.join(profile[1].split("\\")[1:]))
-        # pfp = url_for('static',filename='\\'.join(profile[1].split("\\")[1:]))
-        # print(pfp)
         pfp = '/'.join(profile[1].split("\\"))
-        print(pfp)
         username = profile[2]
         name = profile[3]
         email = profile[4]
@@ -145,19 +132,17 @@ def profile():
         interests = profile[7]
         plan = profile[8].title()
 
-        # have to put posts and bought sold also here
-        # Option to choose the plan
         return render_template("profile.html", login = "1", pfp = pfp, username = username, name = name, email = email, password = password, qualifications = qualifications, interests = interests, plan = plan)
     else:
         signin_form = SigninForm()
-        return render_template("login.html", login = "0")
+        return redirect(url_for('signin', login='0'))
+
 
 @app.route('/post', methods=["POST", "GET"])
 def post():
     if session["Username"] != None:
         post_form = PostForm()
         database_script.execute_userinfo("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, image TEXT, original_poster Text, title TEXT, description TEXT, tags TEXT, liked_by TEXT, to_sell TEXT, price TEXT, owner TEXT)")
-        # Make the form
 
         errors_lst = []
         rows = database_script.view_posts()
@@ -169,8 +154,6 @@ def post():
             else:
                 post_to_create = {"Title": post_form.title.data, "Description": post_form.description.data, "Tags": post_form.tags.data, "To Sell": "no", "Price": ""}
 
-            print(post_to_create)
-
             if rows != [] and rows != None:
                 for row in rows:
                     if post_to_create["Title"] == row[3]:
@@ -178,30 +161,19 @@ def post():
             
             file = request.files['post']
             filename = ""
-            print(file.filename, str(file.filename.split(".")[-1]))
             if str(file.filename.split(".")[-1]) in ["png", "jpg", "jpeg"]:
-                print("Yes! It reached!")
                 filename = "static\\images\\posts\\" + post_to_create["Title"] + "." + str(file.filename.split(".")[-1])
-                print(filename)
                 file.save(filename)
-                print('file uploaded successfully')
             else:
                 errors_lst.append("File: " + "Please upload a file with the extention .png, .jpg or .jpeg only")
 
-            print(errors_lst)
 
                 
-            if len(errors_lst) == 0:                                                                          
-                print(request.files['post'])
+            if len(errors_lst) == 0:
 
                 post_to_create_command = "INSERT INTO posts VALUES(NULL, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format('/'.join(filename.split("\\")), str(session["Username"]), post_to_create["Title"], post_to_create["Description"], post_to_create["Tags"], '', post_to_create["To Sell"], post_to_create["Price"], '')
 
-                # post_to_create_command = "INSERT INTO posts VALUES(NULL, '" + str(filename) + "', '" + str(session["Username"]) + "', '" + post_to_create["Title"] + "', '" + post_to_create["Description"] + "', '" + post_to_create["Tags"] + "', '', '" + post_to_create["To Sell"] + "', '" + post_to_create["Price"] + "', '')"
-
-                # post_to_create_command = "INSERT INTO posts VALUES(NULL, '%s', '%s', '%s', '%s', '%s', '', '%s', '%s', '')" % (filename, str(session["Username"]), post_to_create["Title"], post_to_create["Description"], post_to_create["Tags"], post_to_create["To Sell"], post_to_create["Price"])
-                print(post_to_create_command)
                 database_script.execute_userinfo(post_to_create_command)
-                print(view_posts())
                 return redirect(url_for('index', login="1"))
 
         if post_form.errors != {}:
@@ -211,7 +183,7 @@ def post():
         return render_template("post.html", login = "1", post_form = post_form, errors_lst = errors_lst)
 
     else:
-        return render_template("community.html", login = "0")
+        return redirect(url_for('signin', login='0'))
 
 
 @app.route('/community', methods=["POST", "GET"])
@@ -225,7 +197,6 @@ def community():
         login = "0"
     
     rows = view_posts()
-    print(rows)
     lst = []
     i = -1
     for row in rows:
@@ -235,22 +206,14 @@ def community():
         elif row[7] == "no":
             lst.append({"index": i, "post": row[1], "owner": row[2], "title": row[3], "description": row[4], "tags": row[5], "price": "Not for sale"})
         
-        # lst.append({"post": row[1], "owner": row[2], "title": row[3], "description": row[4], "tags": row[5], "price": "Not for sale"})
 
-    print(lst)
     return render_template("community.html", login = login, rows = lst)
-
-
-# Number of likes
-
-
-
 
 
 @app.route('/signout')
 def signout():
     session["Username"] = None
-    return render_template("community.html", login="0")
+    return redirect(url_for("community", login="0"))
 
 
 if __name__ == "__main__":
